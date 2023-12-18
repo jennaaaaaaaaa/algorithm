5e4332b3f6337dcfef391c2867c199a338caa9ec
jennaaaaaaaaa <111362623+jennaaaaaaaaa@users.noreply.github.com>
2023-12-18 12:08:26 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,10 +1,10 @@
-function solution(x) {  
-    let digits = []
-    let temp = x; 
-    while(temp > 0){
-        let digit = temp%10
-        digits.push(digit)
-        temp = Math.floor(temp / 10);        
-    }
-    return x%digits.reduce((a, b) => (a + b)) === 0 ? true : false
+function solution(x) {
+    let num = x;
+    let sum = 0;
+    do {
+        sum += x%10;
+        x = Math.floor(x/10);
+    } while (x>0);
+
+    return !(num%sum);
 }
