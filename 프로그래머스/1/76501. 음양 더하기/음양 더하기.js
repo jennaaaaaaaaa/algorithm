1d40a2c2dcf4f7807a7a77c018022e1292a3ab85
jennaaaaaaaaa <111362623+jennaaaaaaaaa@users.noreply.github.com>
2023-12-19 09:25:23 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,8 @@
 function solution(absolutes, signs) {
-    var answer = 0
-    for(let i = 0; i < absolutes.length; i++){
-        if (signs[i] === true){
-            answer = answer + absolutes[i]
-        } else {
-            answer = answer - absolutes[i]
-        }
+    var answer = 0;
+    for (let i = 0; i<absolutes.length; i++){
+        signs[i] === true ? answer+=absolutes[i] : answer-=absolutes[i]
     }
-    return answer
+
+    return answer;
 }
