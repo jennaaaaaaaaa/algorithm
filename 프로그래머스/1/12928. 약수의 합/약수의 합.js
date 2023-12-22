60262feef82284fe93f42b67528f1aa48241cb38
jennaaaaaaaaa <111362623+jennaaaaaaaaa@users.noreply.github.com>
2023-12-22 09:48:38 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -2,7 +2,7 @@ function solution(n) {
     var answer = 0;
     for(let i =0; i<=n; i++){
         if (n%i===0){
-            answer = answer + i
+            answer += i
         }
     }
     return answer;
