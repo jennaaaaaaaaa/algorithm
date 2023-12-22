f83bfd0ef54ed3745392a8af1a56500de1c42b7e
jennaaaaaaaaa <111362623+jennaaaaaaaaa@users.noreply.github.com>
2023-12-22 09:35:39 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -0,0 +1,8 @@
+function solution(num1, num2) {
+    if(num1>0 && num1<=100) {
+        if(num2>0 && num2<=100)
+            var answer = (num1 / num2) * 1000
+            return Math.floor(answer)
+    }
+    
+}
